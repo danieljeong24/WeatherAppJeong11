ad8fbaf63d951fddbd91a6c7ca179623a0d62cbd
Daniel Jeong <89224086+danieljeong24@users.noreply.github.com>
2023-08-28 10:56:38 -0600

    BackgroundColorAndText

--- WeatherAppJeong/ContentView.swift
+++ WeatherAppJeong/ContentView.swift
@@ -5,17 +5,29 @@
 //  Created by Daniel Jeong on 8/28/23.
 //
 
+/*  Name: Daniel Jeong
+ *  Assignment: PEX 1
+ * Feature Added:
+ *  Documentation Statement:
+ */
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient (colors: [.blue, .green, .white]), startPoint: .topTrailing, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+             
+            VStack{
+                Text("Colorado Springs, CO")
+                    //order of modifiers matter
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer() //vstack is the whole length of the screen but the rest of the space below the text is free to use
+            }
         }
-        .padding()
     }
 }
 
