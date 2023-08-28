cd21e7e9852a4827ee19f106b36c67c0f5b7829a
Daniel Jeong <89224086+danieljeong24@users.noreply.github.com>
2023-08-28 10:01:38 -0600

    Initial Commit

---
+++ WeatherAppJeong/WeatherAppJeongApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppJeongApp.swift
+//  WeatherAppJeong
+//
+//  Created by Daniel Jeong on 8/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppJeongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
